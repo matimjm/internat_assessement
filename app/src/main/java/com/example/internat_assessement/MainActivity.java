b208aaffac92f44c69053b0462c2b6f87ea167bf
matimjm <mateuszmjm@icloud.com>
2022-12-03 18:12:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/internat_assessement/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.internat_assessement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
