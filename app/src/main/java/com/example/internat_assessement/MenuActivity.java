bb5896f0581e74b5ba5b566f4798dc1b56dfe06f
matimjm <mateuszmjm@icloud.com>
2023-03-28 22:30:12 +0200

    Added a sketch of a chart in MenuActivity (frontend not completed, backend working, but with a local(fake) data and not with the data from firebase)

--- app/src/main/java/com/example/internat_assessement/MenuActivity.java
+++ app/src/main/java/com/example/internat_assessement/MenuActivity.java
@@ -8,19 +8,27 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.PieChart;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -34,7 +42,90 @@ public class MenuActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
 
+        Cartesian cartesian = AnyChart.line();
+
+        cartesian.animation(true);
+
+        cartesian.padding(10d, 20d, 5d, 20d);
+
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("Number of Completed services (over a year)");
+
+        cartesian.yAxis(0).title("Number of Completed services (over a year)");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+
+        //TODO under here fetch the amount of completed services in each month (we would need to add A LOT of test data for it to work)
+        List<DataEntry> seriesData = new ArrayList<>();
+        seriesData.add(new CustomDataEntry("January", 3.6));
+        seriesData.add(new CustomDataEntry("February", 7.1));
+        seriesData.add(new CustomDataEntry("March", 8.5));
+        seriesData.add(new CustomDataEntry("April", 9.2));
+        seriesData.add(new CustomDataEntry("May", 10.1));
+        seriesData.add(new CustomDataEntry("June", 11.6));
+        seriesData.add(new CustomDataEntry("July", 16.4));
+        seriesData.add(new CustomDataEntry("August", 18.0));
+        seriesData.add(new CustomDataEntry("September", 13.2));
+        seriesData.add(new CustomDataEntry("October", 12.0));
+        seriesData.add(new CustomDataEntry("November", 3.2));
+        seriesData.add(new CustomDataEntry("December", 4.1));
+
+        Set set = Set.instantiate();
+        set.data(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        //Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+        //Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
+
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("Completed");
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+        //Line series2 = cartesian.line(series2Mapping);
+        //series2.name("Whiskey");
+        //series2.hovered().markers().enabled(true);
+        //series2.hovered().markers()
+        //        .type(MarkerType.CIRCLE)
+        //        .size(4d);
+        //series2.tooltip()
+        //        .position("right")
+        //        .anchor(Anchor.LEFT_CENTER)
+        //        .offsetX(5d)
+        //        .offsetY(5d);
+
+        //Line series3 = cartesian.line(series3Mapping);
+        //series3.name("Tequila");
+        //series3.hovered().markers().enabled(true);
+        //series3.hovered().markers()
+        //        .type(MarkerType.CIRCLE)
+        //        .size(4d);
+        //series3.tooltip()
+        //        .position("right")
+        //        .anchor(Anchor.LEFT_CENTER)
+        //        .offsetX(5d)
+        //        .offsetY(5d);
+
+        cartesian.legend().enabled(true);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(0d, 0d, 10d, 0d);
+
+        anyChartView.setChart(cartesian);
 
 
 
@@ -57,6 +148,16 @@ public class MenuActivity extends AppCompatActivity implements NavigationView.On
         ///TODO do reports listed in xml file
 
 
+
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+
+        CustomDataEntry(String x, Number value) { //(Number value2, Number value3)
+            super(x, value);
+            //setValue("value2", value2);
+            //setValue("value3", value3);
+        }
 
     }
 
